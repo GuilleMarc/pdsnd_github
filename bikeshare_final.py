e82bcc2f6ed8405958fe751970cd46f90563e097
Guillermo Marcillo <guillemarcillo19xx@gmail.com>
2018-11-20 10:43:27 -0500

    Add comment for data sources and filters

--- bikeshare_final.py
+++ bikeshare_final.py
@@ -6,6 +6,7 @@
 import time
 import pandas as pd
 
+# Create data sources and filters (data, month, day)
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
